1303fd113a4950f07257623aa235bc1e7483f710
schaeferstefan <62297163+schaeferstefan@users.noreply.github.com>
2021-09-07 23:22:09 +0200

    +ajax

--- script.js
+++ script.js
@@ -5,6 +5,26 @@ function test(){
 }
 
 
-$("form[name^='test']").on("submit", function(){
- alert('12345');
+$("form[name^='test']").on("submit", function(event){
+    event.preventDefault(); 
+    var message = $('input[name^="message"]').val(); 
+    var messages = $('div.message');
+
+    var messageHistory = [];
+
+    messages.each(function(index) {
+        messageHistory.push($.trim($(this).text()));
+    });
+
+    alert(messageHistory);
+
+    $.ajax({
+        method: "POST",
+        url: "",
+        data: {
+            message: message,
+            messageHistory: messageHistory
+        }
+    })
+
 });
